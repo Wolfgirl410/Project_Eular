4a3fb2e11bbb2225ebb8343ce10365ee29c15029
Wolfgirl410 <mariamohanjacob@gmail.com>
2025-07-09 13:32:19 +0800

    lalala

--- 3. Largest Prime Factor/main.cpp
+++ 3. Largest Prime Factor/main.cpp
@@ -1,18 +1,53 @@
-#include<stdio.h>
-#include <cstdint> 
-int main(){
-    long long int x;
-    x=600851475143;
-    int y;
-
-    for (; x >= 2; x--) {
-        for (y = 2; y < x; y++)
-            if (x % y == 0) 
-            break;
-
-        if (y == x) {
-            printf("%llu\n", x);
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// A function to find largest prime factor
+long long maxPrimeFactors(long long n)
+{
+    // Initialize the maximum prime factor
+    // variable with the lowest one
+    long long maxPrime = -1;
+ 
+    // Print the number of 2s that divide n
+    while (n % 2 == 0) {
+        maxPrime = 2;
+        n >>= 1; // equivalent to n /= 2
+    }
+  // n must be odd at this point
+     while (n % 3 == 0) {
+        maxPrime = 3;
+        n=n/3;
+    }
+ 
+    // now we have to iterate only for integers
+    // who does not have prime factor 2 and 3
+    for (int i = 5; i <= sqrt(n); i += 6) {
+        while (n % i == 0) {
+            maxPrime = i;
+            n = n / i;
+        }
+      while (n % (i+2) == 0) {
+            maxPrime = i+2;
+            n = n / (i+2);
         }
     }
-    return 0;
+ 
+    // This condition is to handle the case
+    // when n is a prime number greater than 4
+    if (n > 4)
+        maxPrime = n;
+ 
+    return maxPrime;
+}
+ 
+// Driver program to test above function
+int main()
+{
+    long long n = 15;
+    cout << maxPrimeFactors(n) << endl;
+ 
+    n = 25698751364526;
+    cout <<  maxPrimeFactors(n);
+ 
 }
